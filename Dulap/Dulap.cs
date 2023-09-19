abf3f2f2c2a94ef818a7716924466f1e418c6285
amali <amali@DESKTOP-E8S8PN6>
2023-09-19 12:37:16 +0300

    Add project files.

---
+++ Dulap/Dulap.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dulap
+{
+    class Dulap
+    {
+        private double lungime;
+        private double latime;
+        private double inaltime;
+
+        public double Lungime
+        {
+            get { return lungime; }
+            set { lungime = value; }
+        }
+        public double Latime
+        {
+            get { return latime; }
+            set { latime = value; } 
+        }
+        public double Inaltime
+        {
+            get { return inaltime; }
+            set { inaltime = value; }
+
+        }
+
+        public Dulap(double lungime, double latime, double inaltime)
+        {
+            Lungime =lungime;
+            Latime = latime;
+            Inaltime = inaltime;
+        }
+
+      // metoda de calculare a volumului
+
+        public double CalcVolum(double lungime , double latime , double inaltime)
+        {
+            double volum = lungime * latime * inaltime;
+
+            return volum;
+        }
+        public void Text(double lungime , double latime , double inaltime , double volum)
+        {
+            Console.WriteLine($" Dulapul are lungimea =  {lungime} , latimea = {latime} , inaltimea = {inaltime} si volumul  = {volum}");
+        }
+    }
+}
---
+++ Dulap/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+namespace Dulap
+{
+
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string afisare1 = " Tastati lungimea = ";
+
+            Console.WriteLine(afisare1);
+            double lungime = double.Parse(Console.ReadLine());
+
+            string afisare2 = " Tastati latimea = ";
+            Console.WriteLine(afisare2);
+            double latime = double.Parse(Console.ReadLine());
+
+            string afisare3 = " Tastati inaltimea = ";
+            Console.WriteLine(afisare3);
+            double inaltime = double.Parse(Console.ReadLine());
+
+            Dulap dulap = new Dulap(lungime, latime, inaltime);
+
+          dulap.CalcVolum(lungime,latime,inaltime);
+
+            dulap.Text(lungime,latime,inaltime,dulap.CalcVolum(lungime, latime, inaltime));
+
+            Dulap dulap2 = dulap;
+
+            afisare1 = " Tastati lungimea = ";
+
+            Console.WriteLine(afisare1);
+            double lungime1 = double.Parse(Console.ReadLine());
+
+            afisare2 = " Tastati latimea = ";
+            Console.WriteLine(afisare2);
+            double latime1 = double.Parse(Console.ReadLine());
+
+           afisare3 = " Tastati inaltimea = ";
+            Console.WriteLine(afisare3);
+            double inaltime1 = double.Parse(Console.ReadLine());
+
+
+            dulap2.CalcVolum(lungime1, latime1, inaltime1);
+
+            dulap2.Text(lungime1, latime1, inaltime1, dulap.CalcVolum(lungime1, latime1, inaltime1));
+
+
+        }
+
+
+
+    }
+}
